e99d5ff3c602375d423ea60b8a9d041449e6522b
javier <xavixastro@gmail.com>
2020-06-16 23:57:49 -0700

    Memoization minChange

--- memoization_project/lib/problems.js
+++ memoization_project/lib/problems.js
@@ -38,7 +38,15 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
-function minChange(coins, amount, memo = {}) {
+function minChange(coins, amount, memo = {}) { // [1,2,5], 11
+    let options = [];
+    let change = []; 
+    coins.forEach(coin => { //1
+        while (coin < amount && (amount-coin) >= 0) {
+            change.push(coin);
+            amount -= coin;
+        }
+    });
 
 }
 
