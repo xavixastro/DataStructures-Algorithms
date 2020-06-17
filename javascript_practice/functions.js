ba82ade38d6b73c420b85cf4a8b4e05be716e7cc
javier <xavixastro@gmail.com>
2020-06-17 12:11:05 -0700

    JS functions

--- javascript_practice/functions.js
+++ javascript_practice/functions.js
@@ -90,3 +90,130 @@ console.log(average(3, 10, 8)); // 7
 console.log(average(10, 5, 12)); // 9
 console.log(average(6, 20, 40)); // 22
 
+console.log('---------------------')
+
+
+// Write a function `startsWithR` that accepts a string as an argument and returns a boolean indicating
+// whether or not the string starts with 'r' or 'R'.
+
+// let startsWithR = str => str[0] === 'r' || str[0] === 'R';
+let startsWithR = str => ['r', 'R'].includes(str[0]);
+
+console.log(startsWithR("roger that")); // true
+console.log(startsWithR("Row, row, row your boat")); // true
+console.log(startsWithR("slip")); // false
+console.log(startsWithR("Taxicab")); // false
+
+console.log('---------------------')
+
+// Write a function `parity` that accepts a number as an argument. The function should return the
+// string 'even' if the number is even. It should return the string 'odd' if the number is odd.
+
+
+let parity = num => (num % 2 === 0) ? 'even' : 'odd';
+
+console.log(parity(5)); // 'odd'
+console.log(parity(7)); // 'odd'
+console.log(parity(13)); // 'odd'
+console.log(parity(32)); // 'even'
+console.log(parity(10)); // 'even'
+console.log(parity(602348)); // 'even'
+
+console.log('---------------------')
+
+// Write a function `longer` that accepts two strings as arguments. The function should return the
+// string that is longer. If the strings have the same length, then return the first string.
+
+
+let longer = (str1, str2) => (str1.length >= str2.length) ? str1 : str2;
+
+console.log(longer("drum", "piranha")); // 'piranha'
+console.log(longer("basket", "fork")); // 'basket'
+console.log(longer("flannel", "sustainable")); // 'sustainable'
+console.log(longer("disrupt", "ability")); // 'disrupt'
+console.log(longer("bird", "shoe")); // 'bird'
+
+console.log('---------------------')
+
+
+// Write a function `oneOrNone` that accepts two booleans as arguments. The function should return true
+// if exactly one of the arguments is true. If BOTH arguments are true, then it should return false.
+
+let oneOrNone = (val1, val2) => !!(val1 ^ val2)
+
+console.log(oneOrNone(false, false)); // false
+console.log(oneOrNone(true, false)); // true
+console.log(oneOrNone(false, true)); // true
+console.log(oneOrNone(true, true)); // false
+
+console.log('---------------------')
+
+
+// Write a function `endsInLy` that accepts a string as an argument and returns a boolean indicating
+// whether or not the string ends in the substring 'ly'.
+
+// let endsInLy = str => str.slice(-2) === 'ly';
+let endsInLy = str => str.endsWith('ly');
+
+console.log(endsInLy("pretty")); // false
+console.log(endsInLy("instant")); // false
+console.log(endsInLy("analytic")); // false
+console.log(endsInLy("timidly")); // true
+console.log(endsInLy("fly")); // true
+console.log(endsInLy("gallantly")); // true
+
+console.log('---------------------')
+
+
+// Write a function `funnySound` that accepts two strings as arguments. The function should return a
+// new string containing the first three characters of both strings concatenated together.
+
+// You can assume that the arguments are both at least three characters long.
+
+let funnySound = (str1, str2) => str1.slice(0, 3) + str2.slice(0, 3);
+
+console.log(funnySound("tiger", "spoon")); // 'tigspo'
+console.log(funnySound("computer", "phone")); // 'compho'
+console.log(funnySound("skate", "bottle")); // 'skabot'
+console.log(funnySound("frog", "ashtray")); // 'froash'
+
+console.log('---------------------')
+
+
+// Write a function `stringSize` that accepts a string as an argument. The function should return the
+// string 'small' if the argument is shorter than 5 characters, 'medium' if it is exactly 5 characters, and
+// 'large' if it is longer than 5 characters.
+
+let stringSize = str => str.length < 5 ? 'small' : str.length > 5 ? 'large' : 'medium';
+
+console.log(stringSize("cat")); // 'small'
+console.log(stringSize("bell")); // 'small'
+console.log(stringSize("ready")); // 'medium'
+console.log(stringSize("shirt")); // 'medium'
+console.log(stringSize("shallow")); // 'large'
+console.log(stringSize("intelligence")); // 'large'
+
+console.log('---------------------')
+
+
+// Write a function `wackyWord` that accepts two strings as arguments. The function should return a new
+// string containing the first three characters of the first string concatenated with the last two
+// character of the second string.
+
+// You can assume that the first argument has a length of at least three and the second argument has a
+// length of at least two.
+
+let wackyWord = (str1, str2) => str1.slice(0, 3) + str2.slice(-2);
+
+console.log(wackyWord("very", "kindly")); // 'verly'
+console.log(wackyWord("forever", "sick")); // 'forck'
+console.log(wackyWord("cellar", "door")); // 'celor'
+console.log(wackyWord("bagel", "sweep")); // 'bagep'
+
+
+
+
+
+
+
+
