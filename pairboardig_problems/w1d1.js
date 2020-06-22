b9af5adfd6ef2c4b471ec1c6d6fa62629a0c7ad7
javier <xavixastro@gmail.com>
2020-06-22 14:13:09 -0700

    JS pairboarding problems

--- pairboardig_problems/w1d1.js
+++ pairboardig_problems/w1d1.js
@@ -26,7 +26,7 @@
 const robotMoves = str => {
     let xPos = 0;
     let yPos = 0;
-    [...str].forEach(ch => {
+    str.forEach(ch => {
         console.log(ch)
         switch (ch) {
             case 'U': xPos += 1; break;
@@ -40,3 +40,46 @@ const robotMoves = str => {
 
 console.log(robotMoves("UD"));
 console.log(robotMoves("LL"));
+
+
+// A self - dividing number is a number that is divisible by every digit it contains.
+// For example, 128 is a self - dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+// Also, a self - dividing number is not allowed to contain the digit zero.
+
+// Given a lower and upper number bound, output a list of every possible self dividing number, 
+// including the bounds if possible.
+
+// Example
+// Input: left = 1, right = 22
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+
+//Helper method: isSelfDividing(n) => boolean
+//Convert n to string
+//Iterate through each char(num) 
+//if num === 0 || n % num !== 0 return false
+//return true after iterating through all numbers
+
+//main function: findSelfDividingRange(left, right)
+//create a variable to keep track of numbers
+//while left <= right call helper function
+//if helper function return true add number to arr
+//return arr
+
+const isSelfDividing = (num) => {
+    let divisible = true;
+    let digits = num.toString();
+    [...digits].forEach(div => {
+        if (div == 0 || num % div !== 0) divisible = false;
+    })
+    return divisible
+}
+
+const findSelfDividingRange = (left, right) => {
+    let nums = [];
+    while (left <= right) {
+        if (isSelfDividing(left)) nums.push(left);
+        left += 1;
+    }
+    return nums;
+}
