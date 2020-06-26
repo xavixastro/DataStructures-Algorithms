045c8f036af0287a6dfd0cad6862983772114791
javier <xavixastro@gmail.com>
2020-06-25 23:42:24 -0700

    JS merge sort

--- merge_sort_project/lib/merge_sort.js
+++ merge_sort_project/lib/merge_sort.js
@@ -7,10 +7,15 @@ function merge(array1, array2) {
             merged.push(array2.shift())
         }
     }
+    return [...merged, ...array1, ...array2]
 }
 
-function mergeSort(array) {
-
+function mergeSort(array) { //[1, 2, 3, 4, 5] //[1, 2, 3]
+    if (array.length <= 1) return array;
+    let mid = Math.floor(array.length / 2)
+    let left = array.slice(0, mid); //[1]
+    let right = array.slice(mid); //[2, 3]
+    return merge(mergeSort(left), mergeSort(right))
 }
 
 module.exports = {
