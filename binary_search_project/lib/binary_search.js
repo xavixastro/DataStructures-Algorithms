9a423cfa026d658e11fd7a97b4284713e3d59f79
javier <xavixastro@gmail.com>
2020-06-27 21:29:38 -0700

    JS binary search

--- binary_search_project/lib/binary_search.js
+++ binary_search_project/lib/binary_search.js
@@ -1,5 +1,12 @@
-function binarySearch(array, target) {
-
+function binarySearch(array, target) { //[2, 3, 6, 8, 12], 8
+    if (!array.length) return false;
+    let mid = Math.floor(array.length / 2)
+    if (array[mid] === target) return true; 
+    if (array[mid] > target) {
+      return binarySearch(array.slice(0, mid), target);
+    } else {
+      return binarySearch(array.slice(mid + 1), target);
+    }
 }
 
 function binarySearchIndex(array, target) {
