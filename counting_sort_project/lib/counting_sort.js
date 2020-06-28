1400ecbc21df34a5940a07466029fa7fe8491b94
javier <xavixastro@gmail.com>
2020-06-27 21:18:32 -0700

    JS counting sort

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,14 @@
 function countingSort(arr, max) {
-
+    let counter = Array(max+1).fill(0);
+    arr.forEach(el => counter[el]++)
+    let sortedArr = [];
+    for (let i = 0; i <= counter.length; i++) {
+        while (counter[i] > 0) {
+            sortedArr.push(i);
+            counter[i]--;
+        }
+    }
+    return sortedArr;
 }
 
 
