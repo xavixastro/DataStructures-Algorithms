51019576ced16df26d603ec4d44c4e22883b1f27
javier <xavixastro@gmail.com>
2020-06-28 23:05:33 -0700

    JS linked list

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -23,7 +23,8 @@
 // TODO: Implement a Linked List Node class here
 class Node {
     constructor(val) {
-
+        this.value = val;
+        this.next = null;
     }
 
 }
@@ -31,12 +32,23 @@ class Node {
 // TODO: Implement a Singly Linked List class here
 class LinkedList {
     constructor() {
-
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
 
     // TODO: Implement the addToTail method here
     addToTail(val) {
-
+        let node = new Node(val);
+        if (this.length === 0){
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
     }
 
     // TODO: Implement the removeTail method here
