42c595a688d7500a4cdb30c9758565b18110569c
javier <xavixastro@gmail.com>
2020-07-08 16:24:26 -0700

    Tree sum

--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,18 @@
 function treeSum(root) {
+    if (!root) return 0;
 
+    //BF
+    let queue = [root];
+    let sum = 0;
+    while (queue.length) {
+        let node = queue.shift();
+        sum += node.val;
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return sum;
+
+    //DF
 }
 
 
