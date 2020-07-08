06584ab710a4315cbe095af1125c7aac296d8bde
javier <xavixastro@gmail.com>
2020-07-08 16:20:40 -0700

    Breadth first array

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,14 @@
 function breadthFirstArray(root) {
-
+    let queue = [];
+    let arr = [];
+    queue.push(root)
+    while (queue.length) {
+        let node = queue.shift();
+        arr.push(node.val)
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return arr;
 }
 
 module.exports = {
