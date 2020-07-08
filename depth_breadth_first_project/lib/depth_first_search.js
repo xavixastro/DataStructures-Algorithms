6f15b984217842061eeb4a07bf439db301e469e6
javier <xavixastro@gmail.com>
2020-07-07 23:58:04 -0700

    Depth First Search

--- depth_breadth_first_project/lib/depth_first_search.js
+++ depth_breadth_first_project/lib/depth_first_search.js
@@ -1,11 +1,21 @@
-function depthFirstSearch(root, targetVal) {
+function depthFirstSearch(root, targetVal) { //a, 'e'//b, 'e'//d, 'e'
     if (!root) return null;
     if (root.value === targetVal) {
         return root; }
     else {
-        return depthFirstSearch(root.left) || depthFirstSearch(root.rigth)
+        if (root.left) depthFirstSearch(root.left, targetVal)
+        if (root.right) depthFirstSearch(root.rigth, targetVal)
+        return null;
     }
 }
+
+//           a
+//          / \
+//         b   c
+//        / \   \
+//       d   e   f
+
+// expect(depthFirstSearch(a, 'e')).to.equal(e);
 
 
 module.exports = {
