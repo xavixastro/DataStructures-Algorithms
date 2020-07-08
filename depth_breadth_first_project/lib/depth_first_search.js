40b7bdbde28b0998a0b25319d3262df9dbc09182
javier <xavixastro@gmail.com>
2020-07-07 23:59:33 -0700

    Depth First Search

--- depth_breadth_first_project/lib/depth_first_search.js
+++ depth_breadth_first_project/lib/depth_first_search.js
@@ -1,12 +1,24 @@
 function depthFirstSearch(root, targetVal) { //a, 'e'//b, 'e'//d, 'e'
-    if (!root) return null;
-    if (root.value === targetVal) {
-        return root; }
-    else {
-        if (root.left) depthFirstSearch(root.left, targetVal)
-        if (root.right) depthFirstSearch(root.rigth, targetVal)
-        return null;
+    // if (!root) return null;
+    // if (root.value === targetVal) {
+    //     return root; }
+    // else {
+    //     if (root.left) depthFirstSearch(root.left, targetVal)
+    //     if (root.right) depthFirstSearch(root.rigth, targetVal)
+    //     return null;
+    // }
+    let stack = [root];
+
+    while (stack.length) {
+        let node = stack.pop();
+
+        if (node.val === targetVal) return node;
+
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
     }
+
+    return null;
 }
 
 //           a
