a85b4f4b78e847376e047ff1dbd066b87d3defbd
javier <xavixastro@gmail.com>
2020-07-07 23:49:31 -0700

    Depth First Search

--- depth_breadth_first_project/lib/depth_first_search.js
+++ depth_breadth_first_project/lib/depth_first_search.js
@@ -1,5 +1,10 @@
 function depthFirstSearch(root, targetVal) {
-
+    if (!root) return null;
+    if (root.value === targetVal) {
+        return root; }
+    else {
+        return depthFirstSearch(root.left) || depthFirstSearch(root.rigth)
+    }
 }
 
 
