ff99d6ba87324b48b7446ca0e0c5d0bdc139db22
javier <xavixastro@gmail.com>
2020-07-08 16:36:16 -0700

    Tree height

--- depth_breadth_first_project/lib/tree_height.js
+++ depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,11 @@
 function treeHeight(root) {
-
+    if (!root) return -1;
+    if (!root.left && !root.right) return 0;
+    if (treeHeight(root.left) > treeHeight(root.right)){
+        return 1 + treeHeight(root.left);
+    } else {
+        return 1 + treeHeight(root.right);
+    }
 }
 
 
