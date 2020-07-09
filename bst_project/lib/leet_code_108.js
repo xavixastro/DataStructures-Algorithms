9ee04701de836d02d66db1354a36547b0494a23e
javier <xavixastro@gmail.com>
2020-07-09 10:12:17 -0700

    BST leet code

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -1,7 +1,24 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
+ function TreeNode(val, left, right) {
+     this.val = (val===undefined ? 0 : val)
+     this.left = (left===undefined ? null : left)
+     this.right = (right===undefined ? null : right)
+ }
 
-function sortedArrayToBST(nums) {
-
+function sortedArrayToBST(nums) { //[-10,-3,0,5,9] // [-10,-3]
+    if (!nums.length) return null;
+    if (nums.length === 1) return new TreeNode(nums[0]);
+    let midIdx = Math.floor(nums.length / 2);//2 //1
+    let root = new TreeNode(nums[midIdx]);
+    root.left = sortedArrayToBST(nums.slice(0, midIdx));
+    root.right = sortedArrayToBST(nums.slice(midIdx + 1));
+    return root;
 }
+
+//              0           
+//             / \
+//           -3   9
+//           /   /
+//         -10  5
--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -1,7 +1,13 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+function findHeight(root) {
+    if (!root) return -1;
+    return 1 + Math.max(findHeight(root.left), findHeight(root.right));
+}
 
 function isBalanced(root) {
+
+    return findHeight(root.left === root.right)
 
 }
