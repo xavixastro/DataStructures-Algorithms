8e32e66163b06f8e044d444a897992740158dd9a
javier <xavixastro@gmail.com>
2020-07-10 13:52:13 -0700

    JS pairboarding questions

--- pairboardig_questions/1a.js
+++ pairboardig_questions/1a.js
@@ -22,3 +22,28 @@ console.log(digitalRoot(6823)); //1
 console.log(digitalRoot(22)); //4
 console.log(digitalRoot(99)); //9
 console.log(digitalRoot(4)); //4
+
+// caesar_cipher
+// Write a function that takes a message and an increment amount and outputs the same 
+// letters shifted by that amount in the alphabet. 
+// Assume lowercase and no punctuation. Preserve spaces.
+
+const caesarCipher = (message, amount) => {
+    let alpha = 'abcdefghijklmnopqrstuvwxyz';
+    let newMessage = '';
+    for (let i = 0; i < message.length; i++) {
+        let char = message[i];
+        let idx = alpha.indexOf(char);
+        if (idx === -1) { 
+            newMessage += char;
+        } else {
+            newMessage += alpha[(idx + amount) % 26]
+        }
+    }
+    return newMessage;
+}
+
+
+console.log(caesarCipher('middle-outz', 2)); // okffng-qwvb 
+console.log(caesarCipher('defend the east wall of the castle', 1)) // efgfoe uif fbtu xbmm pg uif dbtumf
+
