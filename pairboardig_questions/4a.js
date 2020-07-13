ee3f827cf364a80791631657b9cf3b1fc31739d5
javier <xavixastro@gmail.com>
2020-07-12 23:38:45 -0700

    JS pairboarding questions

--- pairboardig_questions/4a.js
+++ pairboardig_questions/4a.js
@@ -15,3 +15,37 @@ const uniqSubs = str => {
 console.log(uniqSubs('hello'));
 console.log(uniqSubs('aaa'));
 console.log(uniqSubs('mike'));
+
+// largest_contiguous_subsum
+// Given an array of integers (positive and negative) find the largest contiguous 
+// subsum (sum of a subarray).
+
+// You can solve this trivially in O(n**2) time by considering all subarrays. 
+// Try to solve it in O(n) time with O(1) memory.
+
+
+//[5000, -1000, 50, 5000]
+//[100, 5, -1000, 3000]
+//[100, 5, -1000, 100, -5, 100]
+
+const largestContiguousSubsum = arr => { 
+    let largest; //5000
+    let currentSum; 
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i === 0) {
+            largest = arr[i];
+            currentSum = arr[i];
+        } else {
+            currentSum += arr[i];
+            if (currentSum > largest) {
+                largest = currentSum;
+            }
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+    }
+
+    return largest;
+}
