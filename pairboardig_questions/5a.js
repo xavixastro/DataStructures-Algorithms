16b13f7f6f738fe76eac207241acb6e61a11a710
javier <xavixastro@gmail.com>
2020-07-13 23:55:22 -0700

    JS pairboarding problems

--- pairboardig_questions/5a.js
+++ pairboardig_questions/5a.js
@@ -2,18 +2,34 @@
 // Given a matrix of integers and coordinates of a rectangular region within the 
 // matrix, find the sum of numbers falling inside the rectangle.
 
+// [
+//     [2,4,5],
+//     [6,3,6],
+//     [6,1,3]
+// ]
+// [1,1], [2,2]
+
 const matrixRegionSum = (matrix, topLeft, rightBottom) => {
     let sum = 0;
-    let limitX = rightBottom[0];
-    let limitY = rightBottom[1];
-    let x = topLeft[0];
-    let y = topLeft[1];
-    let width = limitY - y;
-    let height = limitX - x;
+    let limitRow = rightBottom[0];
+    let limitCol = rightBottom[1];
+    let startRow = topLeft[0];
+    let startCol = topLeft[1];
 
-
-    while (true) {
-        sum += matrix[x][y];
-        y++;
+    for (let i = startRow; i <= limitRow; i++) {
+        for (let j = startCol; j <= limitCol; j++) {
+            sum += matrix[i][j];
+        } 
     }
+
+    return sum;
 }
+
+let matrix =    [
+                    [2,4,5],
+                    [6,3,6],
+                    [6,1,3]
+                ]
+
+
+console.log(matrixRegionSum(matrix, [1,1], [2,2])); //13
