a0e9ac4a0fc4fe834a25beac47544cbc1f8777a2
javier <xavixastro@gmail.com>
2020-07-14 23:59:35 -0700

    JS pairboarding problems

--- pairboardig_questions/5a.js
+++ pairboardig_questions/5a.js
@@ -39,6 +39,23 @@ console.log(matrixRegionSum(matrix, [1,1], [2,2])); //13
 
 
 const mergerSort = arr = {
+  if (arr.length < 2) return arr;
 
+  middle = arr.count / 2
+  left, right = arr.take(middle), arr.drop(middle)
+
+  sorted_left, sorted_right = merge_sort(left), merge_sort(right)
+
+  merge(sorted_left, sorted_right)
 }
 
+
+def merge(left, right)
+  merged_array = []
+  until left.empty? || right.empty?
+    merged_array <<
+      ((left.first < right.first) ? (left.shift) : (right.shift))
+  end
+
+  merged_array + left + right
+end
---
+++ pairboardig_questions/5b.js
@@ -0,0 +1,2 @@
+// binary_search
+// Implement binary search
