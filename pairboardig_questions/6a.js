169971a840c368a645b76302b77ce5c41632589f
javier <xavixastro@gmail.com>
2020-07-15 16:50:53 -0700

    JS pairboarding problems

--- pairboardig_questions/6a.js
+++ pairboardig_questions/6a.js
@@ -9,7 +9,7 @@
 
 
 const subsets = arr => {  //["b"]
-    debugger
+    // debugger
     if (!arr.length) return [[]];
     if (arr.length === 1) return [[], arr.slice()];
     let ele = arr.shift(); //'a'
@@ -19,3 +19,34 @@ const subsets = arr => {  //["b"]
 }
 
 console.log(subsets(["a", "b", "c"]));
+
+
+// longest_palindrome
+// Write a function that will take a string and return the indices of the start/end 
+// of the longest palindrome it contains.
+
+// Example:
+
+// longest_palindrome('acapella') => [0, 2]
+
+function longestPalindrome(str){
+    let longest = 0;
+    let indices = null;
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i; j < str.length; j++) {
+            let sub = str.slice(i, j + 1);
+            if (isPalindrome(sub) && sub.length > longest) {
+                longest = sub.length; 
+                indices = [i, j];
+            }
+        }
+    }
+    return indices; 
+}
+
+function isPalindrome(str){
+    return str === [...str].reverse().join('');
+}
+
+console.log(longestPalindrome('acapella')) //=> [0, 2]
