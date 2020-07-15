74f4d054b1dbec2207fd3d3846d1ff34b6c19db2
javier <xavixastro@gmail.com>
2020-07-15 11:44:55 -0700

    JS pairboarding problems

--- pairboardig_questions/5b.js
+++ pairboardig_questions/5b.js
@@ -1,2 +1,22 @@
 // binary_search
 // Implement binary search
+
+const binarySearch = (arr, target) => {
+    debugger
+    if (!arr.length) return -1; 
+    let midIdx = Math.floor(arr.length/2);
+    let midEle = arr[midIdx];
+    if (midEle === target) return midIdx;
+    if (midEle > target) {
+        return binarySearch(arr.slice(0, midIdx), target)
+    } else {
+        let result = binarySearch(arr.slice(midIdx + 1), target);
+        return result === -1 ? -1 : midIdx + 1 + result; 
+    }
+}
+
+console.log(binarySearch([2, 3, 6, 9, 11, 12, 13, 28, 56, 99], 1)) //-1
+console.log(binarySearch([2, 3, 6, 9, 11, 12, 13, 28, 56, 99], 12)) //5
+console.log(binarySearch([2, 3, 6, 9, 11, 12, 13, 28, 56, 99], -6)) //-1
+console.log(binarySearch([2, 3, 6, 9, 11, 12, 13, 28, 56, 99], 99)) //9
+console.log(binarySearch([2, 3, 6, 9, 11, 12, 13, 28, 56, 99], 2)) //0
