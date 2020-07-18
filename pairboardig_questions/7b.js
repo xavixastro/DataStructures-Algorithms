08d583e541cd89ca0c48e030fb6d39228f15de27
javier <xavixastro@gmail.com>
2020-07-18 15:48:16 -0700

    JS pairboarding questions

--- pairboardig_questions/7b.js
+++ pairboardig_questions/7b.js
@@ -6,5 +6,16 @@
 // Algorithm should be O(n); use O(1) extra space.
 
 const moveZeros = arr => {
-    
+    let pointer = arr.length - 1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[pointer] != 0) {
+            let sub = arr.splice(pointer, 1);
+            arr.unshift(sub[0]);
+        } else {
+            pointer--;
+        }
+    }
+    return arr;
 }
+
+console.log(moveZeros([1, 2, 0, 3, 4, 0, 5, 6, 0])) //[1, 2, 6, 3, 4, 5, 0, 0, 0]
