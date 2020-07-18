ca1d7e1b0faf948a3b7edfe76c163401a00808f6
javier <xavixastro@gmail.com>
2020-07-17 17:14:44 -0700

    JS pairboarding questions

--- pairboardig_questions/7a.js
+++ pairboardig_questions/7a.js
@@ -27,3 +27,11 @@ const nonComparison2 = arr => {
 let arr = [7, 3, 1, 6, 9, 2, 4, 5, 8]
 // console.log(nonComparison1(arr));
 console.log(nonComparison2(arr));
+
+
+// weighted_random_index
+// Given an array, write a function that will return a random index of the array.
+// The probability of an index being returned is weighted by the value at that index 
+// against the sum of the array values.For example, for the array[4, 6, 8], index 0 
+// should be returned with 4 in 18 odds, index 1 should be returned with 6 in 18 odds, 
+// and index 2 should be return with 8 in 18 odds.Implement this in O(n) time.
