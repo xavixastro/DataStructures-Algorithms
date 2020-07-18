3c291d1d5481b3ef9be4169d3187c674d1a07e0a
javier <xavixastro@gmail.com>
2020-07-17 17:13:45 -0700

    JS pairboarding questions

--- pairboardig_questions/7a.js
+++ pairboardig_questions/7a.js
@@ -8,14 +8,22 @@ const nonComparison1 = arr => {
     return [...arr.concat(undefined).keys()].slice(1)
 }
 
-let arr = [7, 3, 1, 6, 9, 2, 4, 5, 8]
-console.log(nonComparison1(arr));
-
 // Part 2: Say that I give you an array of length n with numbers in the range 1..N(N >= n).
 // Sort this array in O(n + N) time.You may use O(N) memory.
+
+const nonComparison2 = arr => {
+    let sortedArr = Array(arr.length);
+    arr.forEach(el => sortedArr[el-1] = el);
+    return sortedArr;
+}
 
 // Part 3: Say I give you an array of n strings, each of length k.I claim that, using 
 // merge sort, you can sort this in O(knlog(n)), since comparing a pair of strings takes O(k) time.
 
 // I want you to beat that.Sort the strings in O(kn).Hint: do not compare any two strings.
 // You may assume all strings contain only lowercase letters a..z without whitespace or punctuation.
+
+
+let arr = [7, 3, 1, 6, 9, 2, 4, 5, 8]
+// console.log(nonComparison1(arr));
+console.log(nonComparison2(arr));
