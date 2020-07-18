dbad78c421027467bb6e66e3d31e25ee5b587db2
javier <xavixastro@gmail.com>
2020-07-18 15:57:21 -0700

    JS pairboarding questions

--- pairboardig_questions/7b.js
+++ pairboardig_questions/7b.js
@@ -19,3 +19,33 @@ const moveZeros = arr => {
 }
 
 console.log(moveZeros([1, 2, 0, 3, 4, 0, 5, 6, 0])) //[1, 2, 6, 3, 4, 5, 0, 0, 0]
+
+
+// look_and_say
+// Implement the 'look and say' function.'Look and say' takes an input array and 
+// outputs an array that describes the count of the elements in the input array 
+// as they appear in order.
+
+const lookAndSay = arr => {
+    let count = [];
+    let currentCount = 0;
+    let prevEle = null;
+    for (let i = 0; i < arr.length; i++) {
+        if (i === 0 || arr[i] === prevEle){
+            currentCount++;
+        } else {
+            count.push([currentCount, prevEle])
+            currentCount = 1;
+        }
+        prevEle = arr[i];
+    }
+    count.push([currentCount, prevEle])
+    return count;
+
+}
+
+
+console.log(lookAndSay([1])) // [[1, 1]]
+console.log(lookAndSay([1, 1])) // [[2, 1]]
+console.log(lookAndSay([2, 1])) // [[1, 2], [1, 1]]
+console.log(lookAndSay([1, 2, 1, 1])) // [[1, 1], [1, 2], [2, 1]]
