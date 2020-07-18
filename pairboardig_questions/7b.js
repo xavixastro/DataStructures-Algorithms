a51f424a7b1ca5b89f0663419188e082eac802f8
javier <xavixastro@gmail.com>
2020-07-18 15:40:07 -0700

    JS pairboarding questions

---
+++ pairboardig_questions/7b.js
@@ -0,0 +1,10 @@
+// move_zeros
+// Given an array, move all zeros to the end.
+// The order of non - zero elements does not matter.Ex:
+
+// move_zeros([1, 2, 0, 3, 4, 0, 5, 6, 0]) == [1, 2, 6, 3, 4, 5, 0, 0, 0]
+// Algorithm should be O(n); use O(1) extra space.
+
+const moveZeros = arr => {
+    
+}
