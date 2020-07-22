6eb4a4a3bbec448d3e38520e860cfba96d966c2d
javier <xavixastro@gmail.com>
2020-07-21 22:45:25 -0700

    JS pairboarding questions

---
+++ pairboardig_questions/8a.js
@@ -0,0 +1,9 @@
+// Sums Upon Sums
+// I give you a scrambled list of n unique integers between 0 and n.Tell me what number is missing.
+
+// If I let you use O(nlog(n)) time, what is a naive way of doing this ?
+
+// Next, what if I require that you solve the problem in O(n) time ? What datastructure might you use ?
+
+// Finally, how could you solve the problem in O(n), and also O(1) space ?
+
