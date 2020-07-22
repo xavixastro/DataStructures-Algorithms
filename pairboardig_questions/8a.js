b55ac9cf6a73e8f1de8d44dd730cb8f165a3373b
javier <xavixastro@gmail.com>
2020-07-21 22:58:11 -0700

    JS pairboarding questions

--- pairboardig_questions/8a.js
+++ pairboardig_questions/8a.js
@@ -3,7 +3,21 @@
 
 // If I let you use O(nlog(n)) time, what is a naive way of doing this ?
 
+// input: [4, 2, 0, 5, 1] => 3
+
+const sumsUponSums = arr => {
+    let count = Array(arr.length + 1).fill(false);
+    arr.forEach(num => {
+        count[num] = true; 
+    })
+    return count.indexOf(false);
+}
+
+console.log(sumsUponSums([4, 2, 0, 5, 1]));
+
 // Next, what if I require that you solve the problem in O(n) time ? What datastructure might you use ?
 
 // Finally, how could you solve the problem in O(n), and also O(1) space ?
+
+
 
