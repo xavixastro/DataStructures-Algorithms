7ca0333e380a959953b77f5d3ce7f4a598c7ea63
javier <xavixastro@gmail.com>
2020-07-23 22:00:22 -0700

    JS pairboarding questions

---
+++ pairboardig_questions/8b.js
@@ -0,0 +1,9 @@
+// StackQueue
+// Implement a queue using stacks.That is, write enqueue and dequeue using only push and pop operations.
+
+// In terms of performance, enqueue should be O(1), but dequeue may be worst -case O(n).
+// In terms of ammortized time, dequeue should be O(1).Prove that your solution accomplishes this.
+
+class StackQueue {
+    
+}
