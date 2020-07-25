c74bce9838e3a61990546249ebaef653382055c8
javier <xavixastro@gmail.com>
2020-07-24 18:34:55 -0700

    JS pairboarding questions

--- pairboardig_questions/9b.js
+++ pairboardig_questions/9b.js
@@ -4,6 +4,19 @@
 // find which element is missing in the second array.Do this in linear time with constant memory use.
 
 const findMissingNumber = (arr1, arr2) => {
-    
+    let missingNum = null;
+    arr1.forEach (el => {
+        let idx = arr2.indexOf(el);
+        if (idx === -1) {
+            missingNum = el;
+        } else {
+            arr2.splice(idx, 1)
+        }
+    });
+    return missingNum;
 }
 
+let arr1 = [5, 5, 3, 68, 3, 2, 23, 7, 9, 54, 2];
+let arr2 = [2, 7, 23, 3, 68, 54, 5, 5, 2, 3];
+
+console.log(findMissingNumber(arr1, arr2)) //9
