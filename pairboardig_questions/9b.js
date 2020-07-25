fba816ab1084ef8e3b4262f1309fa9c0cffe67aa
javier <xavixastro@gmail.com>
2020-07-24 18:46:22 -0700

    JS pairboarding questions

--- pairboardig_questions/9b.js
+++ pairboardig_questions/9b.js
@@ -26,3 +26,26 @@ let arr1 = [5, 5, 3, 68, 3, 2, 23, 7, 9, 54, 2];
 let arr2 = [2, 7, 23, 3, 68, 54, 5, 5, 2, 3];
 
 console.log(findMissingNumber(arr1, arr2)) //9
+
+
+// is_shuffle ?
+// Given three strings, return whether the third is an interleaving of the first two.
+// Interleaving means it only contains characters from the other two, no more no less, 
+// and preserves their character ordering. "abdecf" is an interleaving of "abc" and "def".
+// Note that the first two strings needn't be in alphabetical order like these.
+
+// You may assume that the first two strings do not contain any characters in common.
+
+// Next, relax the assumption that the first two strings contain no overlap.
+// Analyze the time - complexity of your solution.You may wish to view this problem recursively.
+
+// Example:
+
+// interleaving ? ('XXZ', 'XXY', 'XXYXXZ')
+// => true
+
+// Note: make sure you can answer why this won't work with your initial implementation.
+
+const interleaving = (str1, str2, str3) => {
+    
+}
