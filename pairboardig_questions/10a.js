a04922607c75972328db344cef319a2e3776ef08
javier <xavixastro@gmail.com>
2020-07-25 18:43:30 -0700

    JS pairboarding questions

--- pairboardig_questions/10a.js
+++ pairboardig_questions/10a.js
@@ -22,3 +22,37 @@ console.log(binary(2)); //00000010
 console.log(binary(12)); //00001100
 console.log(binary(16)); //00010000
 
+
+// Factorial
+// Implement factorial with and without recursion.
+// What is a potential disadvantage of the recursive way ?
+
+// What is tail - recursion ? 
+// Does Ruby have tail - call optimization ? 
+// Pretend it did; write a tail - recursive version of rec_fac.
+
+const recursiveFactorial = num => {
+    if (num === 0) return 1;
+    return num * recursiveFactorial(num - 1);
+}
+
+console.log(recursiveFactorial(1));
+console.log(recursiveFactorial(2));
+console.log(recursiveFactorial(3));
+console.log(recursiveFactorial(4));
+console.log(recursiveFactorial(10));
+
+const iterativeFactorial = num => {
+    let result = 1;
+    while (num > 0) {
+        result *= num;
+        num--;
+    }
+    return result
+}
+
+console.log(iterativeFactorial(1));
+console.log(iterativeFactorial(2));
+console.log(iterativeFactorial(3));
+console.log(iterativeFactorial(4));
+console.log(iterativeFactorial(10));
