40f1e7d91eb546c158d45ad7acca651817f9d570
javier <xavixastro@gmail.com>
2020-08-07 18:44:22 -0700

    pairboarding questions

--- pairboardig_questions/10b.js
+++ pairboardig_questions/10b.js
@@ -2,17 +2,23 @@
 // Given a string, find the lexicographically greatest pseudo - substring.
 
 function maxUniquePsub(str){
+    let subs = findSubs(str);
+    return subs.reduce((acc, ele) => acc > ele ? acc : ele);
+}
+
+function findSubs(str) {
     if (!str.length) return [''];
     let subs = [];
     let chr = str[0];
-    let otherSubs = maxUniquePsub(str.slice(1))
+    let otherSubs = findSubs(str.slice(1))
     subs = [...otherSubs];
     otherSubs.forEach(sub => {
         subs.push(chr + sub)
     })
+    subs.reduce((acc, ele) => acc > ele ? acc : ele)
     return subs;
 }
 
 console.log(maxUniquePsub('abcdef')) // => 'f'
-maxUniquePsub('abcdefedcba') // => 'fedcba'
-maxUniquePsub('algorithms') // => 'ts'
+console.log(maxUniquePsub('abcdefedcba')) // => 'fedcba'
+console.log(maxUniquePsub('algorithms')) // => 'ts'
