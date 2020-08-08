eb601af7b3761cb814aed90249f3a7c643e603f7
javier <xavixastro@gmail.com>
2020-08-07 19:07:31 -0700

    pairboarding questions

--- pairboardig_questions/11a.js
+++ pairboardig_questions/11a.js
@@ -2,17 +2,19 @@
 // Write a method that takes an array and returns all its permutations.
 // Time / memory complexity should be proportional to the number of permutations; what is this ?
 
-const permutations = arr => {
+const permutations = arr => { //[1, 2, 3]
     if (!arr.length) return [];
-    let ele = arr[0];
+    if (arr.length === 1) return [arr];
+    let ele = arr[0]; //1
     let perms = [];
-    let otherPermutations = permutations(arr.slice(1));
+    let otherPermutations = permutations(arr.slice(1)); //[[7]]
     otherPermutations.forEach(perm => {
-        for (let i = 0; i < perm.length; i++) {
+        for (let i = 0; i <= perm.length; i++) {
             perms.push([...perm.slice(0, i), ele, ...perm.slice(i)])
         }
     })
     return perms;
 }
 
+// console.log(permutations([1, 2]));
 console.log(permutations([1, 2, 3])) //[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 3, 2], [3, 1, 2], [3, 2, 1]]
