d3d37e40a7cb06349b42ff8b92ffe9f441a9ebc4
javier <xavixastro@gmail.com>
2020-08-07 18:46:46 -0700

    pairboarding questions

---
+++ pairboardig_questions/11a.js
@@ -0,0 +1,6 @@
+// permutations
+// Write a method that takes an array and returns all its permutations.
+// Time / memory complexity should be proportional to the number of permutations; what is this ?
+
+
+permutations([1, 2, 3]) //[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 3, 2], [3, 1, 2], [3, 2, 1]]
