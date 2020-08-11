077d16568eea2c52fce87de5d2194caf18870bd5
javier <xavixastro@gmail.com>
2020-08-10 20:38:00 -0700

    pairboarding questions

--- pairboardig_questions/12a.js
+++ pairboardig_questions/12a.js
@@ -1,8 +1,28 @@
-next_largest
-Given a node in a Binary Search Tree, find the node with the next largest value.
-Assume you don't have the root of the tree, just a single node from it.
+// next_largest
+// Given a node in a Binary Search Tree, find the node with the next largest value.
+// Assume you don't have the root of the tree, just a single node from it.
 
 
 function nextLargest(node){
-    
+    if (node.right){
+        return leftMostNode(node.right)
+    }
+
+    while (true) {
+        let parentNode = node.parent;
+        if (!parentNode) {
+            return null;
+        } else if (parentNode.left === node) {
+            return parentNode;
+        } else {
+            node = parentNode;
+        }
+    }
+}
+
+function leftMostNode(node) {
+    while (node.left) {
+        node = node.left
+    }
+    return node;
 }
