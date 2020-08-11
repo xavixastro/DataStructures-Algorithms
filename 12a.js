ff8af338e5d1d2071e8bbe7556b0f16697375b4b
javier <xavixastro@gmail.com>
2020-08-10 20:28:34 -0700

    pairboarding questions

---
+++ 12a.js
@@ -0,0 +1,8 @@
+next_largest
+Given a node in a Binary Search Tree, find the node with the next largest value.
+Assume you don't have the root of the tree, just a single node from it.
+
+
+function nextLargest(node){
+    
+}
