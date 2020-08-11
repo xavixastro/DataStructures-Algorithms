abb4a006d586ff839d936ccbc20fa5afb7e69ede
javier <xavixastro@gmail.com>
2020-08-10 20:23:26 -0700

    pairboarding questions

---
+++ 11b.js
@@ -0,0 +1,18 @@
+// cyclic ?
+// Consider a linked list. Each link in the list holds a next reference to the 
+// next item in the list, except for the final link, which points to nil.
+
+// It is possible to have a "list" without any end, which loops back on itself.
+
+// Possibilities:
+
+// A -> B -> C -> A -> ...
+// A -> B -> C -> B -> ...
+
+// Write a method cyclic ? (first_link), which will return true if a list is cyclic.
+// Your first version may use O(n) memory.Next, write a version which uses O(1) memory; 
+// you'll probably need a different approach.
+
+function methodCyclic(head) {
+    
+}
