4ff8ed5d553b01973cba0444c15739e7dcdf117b
javier <xavixastro@gmail.com>
2020-08-11 22:22:07 -0700

    pairboarding questions

--- pairboardig_questions/12b.js
+++ pairboardig_questions/12b.js
@@ -2,6 +2,11 @@
 // Given a binary tree, write a function to check whether it’s a binary search tree or not.
 
 
-function isBst(){
-    
+function isBst(node, min = null, max = null){
+    if (node.null) return true;
+
+    if ((min && (min > node.value)) || (max && (max < node.value))) return false
+
+    return isBst(node.left, min, node.value) && isBst(node.right, node.value, max);
+
 }
