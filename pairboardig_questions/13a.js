10626f8c08b3846352f35022946d5d89b8aa4d2b
javier <xavixastro@gmail.com>
2020-08-12 22:04:27 -0700

    pairboarding problems

--- pairboardig_questions/13a.js
+++ pairboardig_questions/13a.js
@@ -2,7 +2,19 @@
 // Write a method to generate a random integer(0...7), given a method that generates 
 // a random integer between(0...5).The resulting rand7 distribution must be uniform.
 
-function rand7(){
-
-    
+function rand5(){
+    return Math.floor(Math.random() * 5);
 }
+
+function rand7(){
+    while (true){
+        //generate rand5
+        num = 5 * rand5() + rand5();
+        if (num < 21) return num % 7;
+    }
+}
+
+console.log(rand7());
+console.log(rand7());
+console.log(rand7());
+console.log(rand7());
