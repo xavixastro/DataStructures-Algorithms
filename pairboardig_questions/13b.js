7534c2e5171f0c5964141ed9cd91aa5f6ae51682
javier <xavixastro@gmail.com>
2020-08-13 23:45:38 -0700

    pairboarding problems

---
+++ pairboardig_questions/13b.js
@@ -0,0 +1,11 @@
+// Square Root
+// Implement a square root function that uses only addition, subtraction, multiplication 
+// and division in less than linear time.You may assume that input is always a perfect square.
+
+// Hint: One naive solution has a better time complexity than many people realize--
+// iterating from 0 until the square root is going to be O(sqrt n), not O(n).However, we can do better!
+
+
+function squareRoot(){
+    
+}
