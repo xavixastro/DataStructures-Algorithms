19a0f9cc5b436f92bd5e4894e439422c964a1301
javier <xavixastro@gmail.com>
2020-08-14 13:49:00 -0700

    pairboarding problems

--- pairboardig_questions/13b.js
+++ pairboardig_questions/13b.js
@@ -6,14 +6,30 @@
 // iterating from 0 until the square root is going to be O(sqrt n), not O(n).However, we can do better!
 
 
-function squareRoot(num){
-    let i = 1
-    while (true) {
-        if (i * i === num) return i;
-        i++;  
-    }
+// function squareRoot(num){
+//     let i = 1
+//     while (true) {
+//         if (i * i === num) return i;
+//         i++;  
+//     }
+// }
+
+function squareRoot(num, candidates = null) {
+    // debugger
+    if (num === 1) return num;
+    candidates = candidates || [...Array(Math.floor(num / 2)+1).keys()].slice(1);
+    let middle = candidates.length / 2;
+    switch (Math.sign(num - (candidates[middle] * candidates[middle]))){
+        case 1:
+            return squareRoot(num, candidates.slice(0, middle))
+        case 0:
+            return candidates[middle];
+        case -1: 
+            return squareRoot(num, candidates.slice(middle + 1))
+    } 
 }
 
-console.log(squareRoot(1));
-console.log(squareRoot(25));
-console.log(squareRoot(49));
+// console.log(squareRoot(1));
+console.log(squareRoot(4));
+// console.log(squareRoot(25));
+// console.log(squareRoot(49));
