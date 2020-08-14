3f7055c7f81d65e9f8327986209d21e849c32ab8
javier <xavixastro@gmail.com>
2020-08-14 14:28:08 -0700

    pairboarding problems

--- pairboardig_questions/14b.js
+++ pairboardig_questions/14b.js
@@ -3,6 +3,14 @@
 // consisting of the values of the array in "spiral order"(top row, then right hand side, 
 // then bottom in reverse, then up, the back again...).
 
-function printSpiral(){
-    
+function printSpiral(matrix){
+    let spiral = matrix[0];
+    for (let i = 1; i < matrix.length; i++) {
+        spiral.push(matrix[i][matrix[i].length - 1])
+    }
+    spiral = spiral.concat(matrix[matrix.length - 1].reverse());
+    for (let i = matrix.length - 1; i > 0; i--) {
+        spiral.push(matrix[i][0])
+    }
+    return spiral;
 }
