6056bdb29f2081e6ef9b0ceaa47cb45e97d437d7
javier <xavixastro@gmail.com>
2020-08-16 21:41:52 -0700

    pairboarding problems

---
+++ pairboardig_questions/15a.js
@@ -0,0 +1,13 @@
+// streaming_sample
+// You know how to use rand to randomly sample an element from an array.
+
+// Now, write a function that, given an input stream of objects, will sample a value.
+// The stream has limited length.
+
+// Use only O(1) memory.
+// Every value in the stream should have an equal probability of being sampled.
+
+
+function streamingSample(){
+    
+}
