98fe38abd23137e04ba9d7296126996606532979
javier <xavixastro@gmail.com>
2020-08-20 11:39:54 -0700

    pairboarding problems

---
+++ pairboardig_questions/w1d5.js
@@ -0,0 +1,14 @@
+// Make Change
+// Write a function that takes in an amount and a set of coins.Return the minimum number 
+// of coins needed to make change for the given amount.You may assume you have an unlimited 
+// supply of each type of coin.If it's not possible to make change for a given amount, return nil or NaN.
+
+// Example:
+
+// make_change(14, [10, 7, 1])
+// # return 2 because[7, 7] is the smallest combination
+
+
+function makeChange(amount, coins){
+
+}
