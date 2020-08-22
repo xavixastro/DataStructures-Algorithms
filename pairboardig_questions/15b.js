b20e945ee758edce0e5fdce1c2dec1f1d0009625
javier <xavixastro@gmail.com>
2020-08-22 13:01:28 -0700

    pairboarding problems

--- pairboardig_questions/15b.js
+++ pairboardig_questions/15b.js
@@ -3,3 +3,10 @@
 // That is, if both arrays were combined, find the median element from the combined array.
 // Assume that there is not enough memory to actually combine both arrays.
 // There exists an O(log n + log m) solution.
+
+// [1,3,3,6,7,8,9] median = 6
+// [1,2,3,4,5,6,8,9] median = 4.5 = (4 + 5) % 2
+
+function median(arr1, arr2) {
+
+}
