46b46ea39e7e79f2c9aea87ca817d47f7ac38483
javier <xavixastro@gmail.com>
2020-08-23 09:53:27 -0700

    pairboarding problems

--- pairboardig_questions/15b.js
+++ pairboardig_questions/15b.js
@@ -6,7 +6,14 @@
 
 // [1,3,3,6,7,8,9] median = 6
 // [1,2,3,4,5,6,8,9] median = 4.5 = (4 + 5) % 2
+// [1,1,2,3,3,3,4,5,6,6,7,8,8,9,9] median = 5
 
 function median(arr1, arr2) {
+
+    let median = null;
+    let smallest = arr1[0] < arr2[0] ? arr1.shift() : arr2.shift();
+    let highest = arr1[arr1.length - 1] > arr2[arr2.length - 1] ? arr1.pop() : arr2.pop();
+
+    
 
 }
