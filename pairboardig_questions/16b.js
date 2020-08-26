5730b0ec4166b27ee5c115eedab0a6156ffdd3b6
javier <xavixastro@gmail.com>
2020-08-26 09:32:10 -0700

    pairboarding problems

---
+++ pairboardig_questions/16b.js
@@ -0,0 +1,15 @@
+// Connected Components
+// You are given a file which looks like so:
+
+// AA BB
+// DD FF
+// CC EE
+// EE DD
+// Each line of the file contains a pair of strings.Each string represents is the 
+// name of a vertex.The line represents an edge connecting two vertices.
+
+// Your task is to find the connected components of the graph.A connected component 
+// is a subset of vertices all connected to each other.In this example, the connected 
+// components are[["AA", "BB"], ["CC", "DD", "EE", "FF"]].
+
+// You don't have to return the elements of the components in any particular order.
