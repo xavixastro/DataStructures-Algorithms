d4aa12858a438d217ec0a1e3714bc20fd3ea0823
javier <xavixastro@gmail.com>
2020-08-26 09:36:54 -0700

    pairboarding problems

--- pairboardig_questions/16b.js
+++ pairboardig_questions/16b.js
@@ -13,3 +13,39 @@
 // components are[["AA", "BB"], ["CC", "DD", "EE", "FF"]].
 
 // You don't have to return the elements of the components in any particular order.
+
+
+function connectedComponents(){
+
+    // ***RUBY SOLUTION***
+
+//     lines = File.readlines(FILE_NAME)
+
+//     matrix = {}
+//     lines.each do | line |
+//         v1, v2 = line.split(" ")
+//   matrix[v1] ||=[]
+//     matrix[v1] << v2
+//     matrix[v2] ||=[]
+//     matrix[v2] << v1
+//     end
+
+//     components = []
+//     until matrix.empty ?
+//         component = []
+
+//   first_key = matrix.keys.first
+//     queue = [first_key]
+//     until queue.empty ?
+//         key = queue.shift
+//     next unless matrix.has_key ? (key)
+//     neighbors = matrix.delete(key)
+
+//     component << key
+//     queue.concat(neighbors)
+//     end
+
+//     components << component
+//     end
+
+}
